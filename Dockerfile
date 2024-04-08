99ffeab48dfb49135fbe0cb837b7b5563609e2ca
vboxuser <vboxuser@Laborator.myguest.virtualbox.org>
2024-04-08 14:06:38 +0300

    laborator

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.11-slim
+WORKDIR /app
+COPY . .
+EXPOSE 8000
+RUN pip install -r requirements.txt
+
+
+#docker stop api; docker rm api
+# build docker
+#docker build -t api .
+
+# pornire imagine
+#docker images
+
+ 
+#docker run -it -d -p 8000:8000 --name api api uvicorn main:app 
+#docker run -it -d -p 8000:8000 --name api api python -m uvicorn main:app --host 0.0.0.0
---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from typing import Union
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from pyod.models.knn import KNN
+from sklearn.model_selection import train_test_split
+
+app = FastAPI()
+
+neigh = None
+clf = None
+
+
+@app.on_event("startup")
+def load_train_model():
+    df = pd.read_csv("iris_ok.csv")
+    global neigh
+    neigh = KNeighborsClassifier(n_neighbors=len(np.unique(df['y'])))
+    neigh.fit(df[df.columns[:4]].values.tolist(), df['y'])
+    print(f'MOdel finished the training')
+
+    global clf
+    clf = KNN()
+    X_train, X_test, y_train, y_test = train_test_split(df[df.columns[:4]].values.tolist(), df['y'], test_size=0.33, shuffle=True, random_state=42)
+    clf.fit(X_train, y_train)
+    print("Incercarea de anomalie")
+
+@app.get("/predict")
+def predict(p1: float, p2: float, p3: float, p4: float):
+    pred = neigh.predict([[p1, p2, p3, p4]])
+    return "{}".format(pred[0])
+
+
+@app.get("/anomalie")
+def detect_anomaly(p1: float, p2: float, p3: float, p4: float):
+    pred = neigh.predict([[p1, p2, p3, p4]])
+    return "{}".format(pred[0])
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
